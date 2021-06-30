a0e3cfaff6bae7639552f655d0e64cb04ab076c7
Zibran Zarif <zzamio1112@gmail.com>
2021-06-30 19:47:39 +0600

    Emulator activity

--- app/src/main/java/com/salikoon/emulator8086/EmulateActivity.java
+++ app/src/main/java/com/salikoon/emulator8086/EmulateActivity.java
@@ -1,13 +1,22 @@
 package com.salikoon.emulator8086;
 
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class EmulateActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_emulator_options, menu);
+        return true;
+    }
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,10 +29,15 @@ public class EmulateActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId()==android.R.id.home) {
-            finish();
-            return true;
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            case R.id.view_mode:
+                Toast.makeText(this, "Hex/Bin/Dec", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-        return super.onOptionsItemSelected(item);
     }
 }
